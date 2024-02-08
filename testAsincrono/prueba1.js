8811590972492639b2d522bbd1467aa2d1f0a764
María N <116673375+MNblue@users.noreply.github.com>
2024-02-08 22:05:02 +0100

    Create prueba1.js

---
+++ testAsincrono/prueba1.js
@@ -0,0 +1,20 @@
+
+//EJEMPLO 1
+// setTimeout(function(){
+
+//     console.log("Hola Mundo");
+
+// },2000);
+
+// console.log("setTimeout() Ejemplo....");
+
+
+
+//EJEMPLO 2
+function saludos(){
+    console.log("Hola Mundo");
+}
+
+setTimeout(saludos, 3000);
+
+console.log("Ejemplo: ");
